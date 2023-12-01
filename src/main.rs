6967d3e4418683fb980b5d5815840fc74ec60e35
Dylan Wright <wrightrex@hotmail.com>
2023-12-01 20:14:52 +0800

    First day complete!

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate aoc_2023;
+extern crate aoc_runner;
+extern crate aoc_runner_derive;
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = aoc_2023 }
+
+// Check https://github.com/gobanos/cargo-aoc
